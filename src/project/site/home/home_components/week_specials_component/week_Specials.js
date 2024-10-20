56bbb75c4cd811834a2ce522b5a1006ff0dff495
Sasha Nekto <bidukoleksandr9@gmail.com>
2024-10-20 23:34:40 +0200

    Add image from S3 storage

--- src/project/site/home/home_components/week_specials_component/week_Specials.js
+++ src/project/site/home/home_components/week_specials_component/week_Specials.js
@@ -1,6 +1,7 @@
 const WeekSpecials = () => {
     return(
         <div>
+            <img src="https://little-lemon-storage.s3.eu-north-1.amazonaws.com/-za4YHM6TEGRVVarcLRCLA_74f4dbcdd1194aa9b1cc56239b41c5e1_image.png?response-content-disposition=inline&X-Amz-Security-Token=IQoJb3JpZ2luX2VjEBMaCmV1LW5vcnRoLTEiRjBEAiBzM6eG1P4BpxBQ9sRk4%2F99OeW%2B9vNxxRIPqt7jvNJ5mwIgWLPxQWfG%2BcKxr5XRHaUlLWrraZ5nphPVwD9U83KToAoqlQIIfRAAGgw3MTM4ODE4MDUzNTYiDC5L4rlmxGeDUiLiTCryAe43T13o%2BnuptRoUakK8NDhq3AbO2%2Bi9A2pPrE5uKFgISZJE%2BAS9F95ocLI6ElaD4dw8Mb3MT7glbDwwTi4jJU7DXMzcLK%2FU7sqQBk5Dc%2FEdAeBJaSdAJpLoNS0RdU3tvwnmWQqu411NORcH0GBljC0cN1gAImnDEO3XApO2CXX%2Bhbezhkb8m0o4N4f6EZTNcB82uWWy2FTk86qEXyIl95MHVZV62dGCHYBfDvP58RojTQdpPjZvCWlrbmw1MC97S1kcm8J3G%2BRgEeumGXHJwG9PQtIQOqfBR0BECS%2BhxRXNTgmiOFu5ryMB7WZwBByV3aBNMN6o1bgGOuABEnYwKy5JuQV1mkcVSpaYDCoepfvexl8G%2F8iEh2f42GYb%2FLTXjrmllJ4Muz8gEKcglo0sWDrU%2FKtytAxUr52BEOuc5VvgbaNn3Ph9eDGF8VpDrZZVaTZhqG2ZK6Ry4q%2BMQ5Jk%2Fu10mff8IXbPJi%2BW9AmBMOxZYYfbe5PJSs7uT0HQwKx%2BhKIA4ixt3NnLtv1wwd4bwQ3R5Nlaya3riTze2l593Kvti6SFmg2CLkBVhG4EaAy3Jq2Rovc4hH1g67ILvEegxhj73Bz0xeP9ZZXAfN%2BXK4p8xVMCKi%2B1dg6ZtWI%3D&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20241020T213057Z&X-Amz-SignedHeaders=host&X-Amz-Expires=300&X-Amz-Credential=ASIA2MNVL4IWAPML54Q2%2F20241020%2Feu-north-1%2Fs3%2Faws4_request&X-Amz-Signature=822b0f901aa1d33a475d54e018f25d849ad781af409014c78e260ec3ad640c0f" alt="Image" />
 
         </div>
     )
